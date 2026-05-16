9dfe6561a510cd3eac98b89dd86fc586b06d147b
AnahiGuanotasig <guanotasig.anahi1n9@gmail.com>
2026-05-15 22:02:13 -0500

    Actualizacion plantas.service.js
    Concluida la logica de negocio para la validacion de datos.

--- src/modules/plantas/plantas.servise.js
+++ src/modules/plantas/plantas.servise.js
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import * as PlantasRepository from "./plantas.repository"
 
+//Contrato para la validacion de datos
 const plantaSchema = z.object({
     codigo: z.string().min(1, "El código es obligatorio"),
     nombre: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
@@ -11,7 +12,8 @@ const plantaSchema = z.object({
     intervalo_riego_dias: z.number().min(1, "El intervalo debe ser al menos 1 día"),
     id_usuario: z.number().int().positive("El ID de usuario es obligatorio")
 });
-export const createPlanta  = async(data) =>{
+
+export const createPlantas = async(data) =>{
     try{
         const validateData = plantaSchema.parse(data);
         return await PlantasRepository.createPlantas(validateData);
@@ -21,4 +23,46 @@ export const createPlanta  = async(data) =>{
         }
         throw new Error("Error al crear la planta: " + error.message);
     }
+};
+
+export const getAllPlantas = async() =>{
+    try {
+        return await PlantasRepository.getAllPlantas();
+    } catch (error) {
+        throw new Error ("Error al obtener las plantas")
+    }
+};
+
+export const getPlanta = async(id) => {
+    try {
+        const planta = await PlantasRepository.getPlanta(id);
+        if (!planta) {
+            throw new Error("La planta solicitada no existe");
+        }
+        return planta;
+    } catch (error) {
+        throw new Error(error.message || "Error al obtener la planta");
+    }
+};
+
+export const updatePlanta = async(id, camposActualizados) =>{
+    try {
+        const updateSchema = plantaSchema.partial();
+        const validateData = updateSchema.parse(camposActualizados);
+
+        return await PlantasRepository.updatePlanta(id,validateData);
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            throw new Error(error.errors[0].message);
+        }
+        throw error;
+    }
+};
+
+export const deletePlanta = async(id) =>{
+    try{
+        return await PlantasRepository.deletePlanta(id);
+    }catch (error) {
+        throw new Error ("Error al eliminar la planta")
+    };
 };
